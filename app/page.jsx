16be8721ee939d580c0ad5fa57728915d87f15bf
root <root@SEMFA-2551.prefeitura.local>
2024-08-09 16:28:38 -0300

    Criando o componente de photo

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -8,11 +9,10 @@ const Home = () => {
 		<section className="h-full">
 			<div className="container mx-auto h-full">
 				<div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-					<div className="text-center xl:text-left">
+					<div className="text-center xl:text-left order-2 xl:order-none">
 						<span className="text-wl">Software Engineer</span>
 						<h1 className="h1">
-							Hello I'm <br />{" "}
-							<span className="text-accent">Gustavo Cescon</span>
+							Hello I'm <br /> <span className="text-accent">Gustavo</span>
 						</h1>
 						<p className="mx-w-[500px] mb-9 text-white/80">
 							I excel at crafting elegant digital experiences and I am
@@ -38,11 +38,12 @@ const Home = () => {
 							</div>
 						</div>
 					</div>
-					<div>
+					<div className="order-1 xl:order-none mb-8 xl:mb-0">
 						<Photo />
 					</div>
 				</div>
 			</div>
+			<Stats />
 		</section>
 	);
 };

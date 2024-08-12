10e4376502d44c8a7656439a20b3e38f2c6ceb40
root <root@SEMFA-2551.prefeitura.local>
2024-08-12 10:05:46 -0300

    Finalizando a página de services

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,59 @@
+"use client";
+
+const about = {
+	title: "About Me",
+	description:
+		"Lorem ipsum dolor sit amet. Ut voluptatem enim ut voluptatem facilis qui aperiam autem in dolorem obcaecati vel illum provident ut possimus tempora ea debitis esse. Eos laboriosam voluptas",
+	info: [
+		{
+			fieldName: "Name",
+			fieldValue: "John Doe",
+		},
+		{
+			fieldName: "Phone",
+			fieldValue: "John Doe",
+		},
+		{
+			fieldName: "Experience",
+			fieldValue: "2+ years",
+		},
+		{
+			fieldName: "Nationality",
+			fieldValue: "Brasileiro",
+		},
+		{
+			fieldName: "Email",
+			fieldValue: "gustavocescon@gmail.com",
+		},
+		{
+			fieldName: "FreeLancer",
+			fieldValue: "Available",
+		},
+		{
+			fieldName: "Languages",
+			fieldValue: "English, Portuguese",
+		},
+	],
+};
+
+const experience = {
+	icon: "/assets/resume/badge.svg",
+	title: "My experience",
+	description:
+		"Lorem ipsum dolor sit amet. Ut voluptatem enim ut voluptatem facilis qui aperiam autem in dolorem obcaecati vel illum provident ut possimus tempora ea debitis esse. Eos laboriosam voluptas",
+	items: [
+		{
+			company: "Company 1",
+			position: "Frontend Developer",
+			duration: "2019 - 2020",
+		},
+		{
+			company: "Company 1",
+			position: "Frontend Developer",
+			duration: "2019 - 2020",
+		},
+	],
+};
 const Resume = () => {
 	return <div>Resume page</div>;
 };
